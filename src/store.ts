94d809f28e74ba4859bd276b360d2a2be2aa3370
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-05-11 22:29:23 +0500

    refactor and fixes

--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,26 @@
-import {EntityManager, FindOptionsWhere, In} from 'typeorm'
+import {EntityManager, FindOptionsWhere, In, FindOptionsRelations} from 'typeorm'
 import assert from 'assert'
 import {Entity as _Entity, EntityClass, FindManyOptions, FindOneOptions, Store} from '@subsquid/typeorm-store'
 import {Graph} from 'graph-data-structure'
 import {def} from '@subsquid/util-internal'
+import {copy} from 'copy-anything'
 
 export interface Entity extends _Entity {
     [k: string]: any
 }
 
-export type DeferList = Map<string, Set<string>>
+export type DeferData<E extends Entity> = {ids: Set<string>; relations: FindOptionsRelations<E>}
 export type CacheMap<E extends Entity> = Map<string, Map<string, E | null>>
 
 export class StoreWithCache {
     private em: EntityManager
 
-    private deferList: DeferList = new Map()
+    private deferMap: Map<string, DeferData<any>> = new Map()
     private cacheMap: CacheMap<any> = new Map()
     private classes: Map<string, EntityClass<any>> = new Map()
 
-    private insertList: Map<string, Map<string, Entity>> = new Map()
-    private upsertList: Map<string, Map<string, Entity>> = new Map()
+    private insertMap: Map<string, Map<string, Entity>> = new Map()
+    private upsertMap: Map<string, Map<string, Entity>> = new Map()
 
     constructor(private store: Store) {
         this.em = (this.store as any).em()
@@ -32,21 +33,18 @@ export class StoreWithCache {
         if (entities.length == 0) return
 
         const entityName = entities[0].constructor.name
-        const _deferList = this.getDeferList(entityName)
         const _insertList = this.getInsertList(entityName)
         const _upsertList = this.getUpsertList(entityName)
         const _cacheMap = this.getCacheMap(entityName)
         for (const entity of entities) {
-            _deferList.delete(entity.id)
-
             assert(!_insertList.has(entity.id))
             assert(!_upsertList.has(entity.id))
 
-            const cached = _cacheMap.get(entity.id)
+            let cached = _cacheMap.get(entity.id)
             assert(cached == null)
 
-            _insertList.set(entity.id, entity)
-            _cacheMap.set(entity.id, entity)
+            cached = await this.cache(entity)
+            _insertList.set(cached.id, cached)
         }
     }
 
@@ -57,18 +55,13 @@ export class StoreWithCache {
         if (entities.length == 0) return
 
         const entityName = entities[0].constructor.name
-        const _deferList = this.getDeferList(entityName)
         const _insertList = this.getInsertList(entityName)
         const _upsertList = this.getUpsertList(entityName)
-        const _cacheMap = this.getCacheMap(entityName)
         for (const entity of entities) {
-            _deferList.delete(entity.id)
-
-            if (!_insertList.has(entity.id)) {
-                _upsertList.set(entity.id, entity)
+            const cached = await this.cache(entity)
+            if (!_insertList.has(cached.id)) {
+                _upsertList.set(cached.id, cached)
             }
-
-            _cacheMap.set(entity.id, entity)
         }
     }
 
@@ -102,7 +95,13 @@ export class StoreWithCache {
 
     async find<E extends Entity>(entityClass: EntityClass<E>, options?: FindManyOptions<E>): Promise<E[]> {
         await this.flush(entityClass)
-        return await this.store.find(entityClass, options).then((v) => this.cache(v))
+        const _deferData = this.getDeferData(entityClass.name)
+        options = options || {}
+        options.relations =
+            options.relations != null ? mergeRelataions(options.relations, _deferData.relations) : _deferData.relations
+        const res = await this.store.find(entityClass, options)
+        await this.cache(res)
+        return res
     }
 
     async findBy<E extends Entity>(
@@ -110,12 +109,19 @@ export class StoreWithCache {
         where: FindOptionsWhere<E> | FindOptionsWhere<E>[]
     ): Promise<E[]> {
         await this.flush(entityClass)
-        return await this.store.findBy(entityClass, where).then((v) => this.cache(v))
+        const res = await this.store.findBy(entityClass, where)
+        await this.cache(res)
+        return res
     }
 
     async findOne<E extends Entity>(entityClass: EntityClass<E>, options: FindOneOptions<E>): Promise<E | undefined> {
         await this.flush(entityClass)
-        return await this.store.findOne(entityClass, options).then((v) => v && this.cache(v))
+        const _deferData = this.getDeferData(entityClass.name)
+        options.relations =
+            options.relations != null ? mergeRelataions(options.relations, _deferData.relations) : _deferData.relations
+        const res = await this.store.findOne(entityClass, options)
+        if (res != null) await this.cache(res)
+        return res
     }
 
     async findOneBy<E extends Entity>(
@@ -123,7 +129,9 @@ export class StoreWithCache {
         where: FindOptionsWhere<E> | FindOptionsWhere<E>[]
     ): Promise<E | undefined> {
         await this.flush(entityClass)
-        return await this.store.findOneBy(entityClass, where).then((v) => v && this.cache(v))
+        const res = await this.store.findOneBy(entityClass, where)
+        if (res != null) await this.cache(res)
+        return res
     }
 
     async findOneOrFail<E extends Entity>(entityClass: EntityClass<E>, options: FindOneOptions<E>): Promise<E> {
@@ -142,12 +150,14 @@ export class StoreWithCache {
         optionsOrId: string | FindOneOptions<E>
     ): Promise<E | undefined> {
         if (typeof optionsOrId === 'string') {
-            await this.load()
+            await this.load(entityClass)
             const id = optionsOrId
             const _cacheMap = this.getCacheMap(entityClass.name)
             const entity = _cacheMap.get(id)
             if (entity !== undefined) {
-                return entity ? structuredClone(entity as E) : undefined
+                if (entity == null) return undefined
+
+                return copy(entity) as E
             } else {
                 await this.flush(entityClass)
                 return await this.store.get(entityClass, id).then((v) => v && this.cache(v))
@@ -165,19 +175,17 @@ export class StoreWithCache {
         return await this.get(entityClass, optionsOrId).then(assertNotNull)
     }
 
-    // defer<T extends Entity>(entityClass: EntityClass<T>, idOrList: string): DeferredValue<T | T[]> // defer<T extends Entity>(entityClass: EntityClass<T>, ids: string[]): DeferredValue<T[]>
-    defer<T extends Entity>(entityClass: EntityClass<T>, id: string): DeferredValue<T> {
+    defer<E extends Entity>(
+        entityClass: EntityClass<E>,
+        id: string,
+        relations?: FindManyOptions<E>['relations']
+    ): DeferredValue<E> {
         this.classes.set(entityClass.name, entityClass)
 
-        // const ids = Array.isArray(idOrList) ? idOrList : [idOrList]
-
-        const _deferredList = this.getDeferList(entityClass.name)
-        const _cacheMap = this.getCacheMap(entityClass.name)
-        // for (const id of ids) {
-        if (!_cacheMap.has(id)) {
-            _deferredList.add(id)
-        }
-        // }
+        const _deferredList = this.getDeferData(entityClass.name)
+        _deferredList.ids.add(id)
+        _deferredList.relations =
+            relations != null ? mergeRelataions(_deferredList.relations, relations) : _deferredList.relations
 
         return new DeferredValue(this, entityClass, id)
     }
@@ -186,7 +194,6 @@ export class StoreWithCache {
         const entityOrder = await this.getTopologicalOrder()
 
         for (const name of entityOrder) {
-            const _cacheMap = this.getCacheMap(name)
             const _insertList = this.getInsertList(name)
             if (_insertList.size > 0) {
                 const entities = _insertList.values()
@@ -205,23 +212,19 @@ export class StoreWithCache {
         }
     }
 
-    private async load(): Promise<void> {
-        for (const [name, _deferList] of this.deferList) {
-            if (_deferList.size === 0) continue
+    private async load<E extends Entity>(entityClass: EntityClass<E>): Promise<void> {
+        const _deferData = this.getDeferData<E>(entityClass.name)
+        if (_deferData.ids.size === 0) return
 
-            const entityClass = this.classes.get(name)
-            assert(entityClass != null)
-
-            await this.find(entityClass, {where: {id: In([..._deferList])}})
-
-            const _cacheMap = this.getCacheMap(name)
-            for (const id of _deferList) {
-                if (_cacheMap.has(id)) continue
-                _cacheMap.set(id, null)
-            }
-
-            _deferList.clear()
+        const _cacheMap = this.getCacheMap(entityClass.name)
+        for (const id of _deferData.ids) {
+            if (_cacheMap.has(id)) continue
+            _cacheMap.set(id, null)
         }
+
+        await this.find<any>(entityClass, {where: {id: In([..._deferData.ids])}, relations: _deferData.relations})
+
+        this.deferMap.delete(entityClass.name)
     }
 
     private cache<E extends Entity>(entity: E): Promise<E>
@@ -230,23 +233,40 @@ export class StoreWithCache {
         const entities = Array.isArray(e) ? e : [e]
         if (entities.length == 0) return
 
-        const _deferList = this.getDeferList(entities[0].constructor.name)
         const _cacheMap = this.getCacheMap(entities[0].constructor.name)
+        const caachedEntities: Entity[] = []
         for (const entity of entities) {
-            _deferList.delete(entity.id)
-            _cacheMap.set(entity.id, entity)
+            const constructor = entity.constructor as any
+            const cached = _cacheMap.get(entity.id) || (new constructor() as Entity)
 
-            const _em = await this.em
-            const metadata = _em.connection.entityMetadatasMap.get(entity.constructor)
-            assert(metadata != null)
-            for (const relation of metadata.relations) {
-                const value = entity[relation.propertyName]
-                if (value == null) continue
-                await this.cache(value)
+            const metadata = this.em.connection.entityMetadatasMap.get(entity.constructor)
+            assert(metadata != null, `Missing metadata for ${entity.constructor.name}`)
+            for (const column of metadata.columns) {
+                const propertyName = column.propertyName
+                if (column.relationMetadata) {
+                    const relationMetadata = column.relationMetadata
+                    const relationPropertyName = relationMetadata.propertyName
+                    if (relationPropertyName in entity) {
+                        assert(!relationMetadata.isOneToMany, `OneToMany relations can't be cached`)
+                        cached[relationPropertyName] =
+                            entity[relationPropertyName] == null
+                                ? undefined
+                                : await this.cache(entity[relationPropertyName])
+                    }
+
+                    if (propertyName != relationPropertyName) {
+                        cached[propertyName] = entity[propertyName]
+                    }
+                } else {
+                    cached[propertyName] = entity[propertyName]
+                }
             }
+
+            _cacheMap.set(cached.id, cached)
+            caachedEntities.push(cached)
         }
 
-        return Array.isArray(e) ? entities : entities[0]
+        return Array.isArray(e) ? caachedEntities : caachedEntities[0]
     }
 
     @def
@@ -261,11 +281,11 @@ export class StoreWithCache {
         return graph.topologicalSort().reverse()
     }
 
-    private getDeferList(name: string) {
-        let list = this.deferList.get(name)
+    private getDeferData<E extends Entity>(name: string): DeferData<E> {
+        let list = this.deferMap.get(name)
         if (list == null) {
-            list = new Set()
-            this.deferList.set(name, list)
+            list = {ids: new Set(), relations: {}}
+            this.deferMap.set(name, list)
         }
 
         return list
@@ -282,20 +302,20 @@ export class StoreWithCache {
     }
 
     private getInsertList(name: string): Map<string, Entity> {
-        let list = this.insertList.get(name)
+        let list = this.insertMap.get(name)
         if (list == null) {
             list = new Map()
-            this.insertList.set(name, list)
+            this.insertMap.set(name, list)
         }
 
         return list
     }
 
     private getUpsertList(name: string): Map<string, Entity> {
-        let list = this.upsertList.get(name)
+        let list = this.upsertMap.get(name)
         if (list == null) {
             list = new Map()
-            this.upsertList.set(name, list)
+            this.upsertMap.set(name, list)
         }
 
         return list
@@ -305,6 +325,29 @@ export class StoreWithCache {
 function assertNotNull<T>(val: T | null | undefined): T {
     assert(val != null)
     return val
+}
+
+function mergeRelataions<E extends Entity>(
+    a: FindOptionsRelations<E>,
+    b: FindOptionsRelations<E>
+): FindOptionsRelations<E> {
+    const mergedObject: FindOptionsRelations<E> = {}
+
+    for (const key in a) {
+        mergedObject[key] = a[key]
+    }
+
+    for (const key in b) {
+        const bValue = b[key]
+        const value = mergedObject[key]
+        if (typeof bValue === 'object') {
+            mergedObject[key] = (typeof value === 'object' ? mergeRelataions(value, bValue) : bValue) as any
+        } else {
+            mergedObject[key] = value || bValue
+        }
+    }
+
+    return mergedObject
 }
 
 export class DeferredValue<E extends Entity> {
